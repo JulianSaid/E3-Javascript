937498f23d124b2982479dd7aa35933dddffb773
Julian Said <juliansaid6@gmail.com>
2023-08-14 20:07:15 -0300

    arrancando

---
+++ index.js
@@ -0,0 +1,86 @@
+const pizzas = [
+  {
+    id: 1,
+    nombre: "pizza de Muzzarella",
+    precio: 500,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+    imagen: "./img/muzzarella.png",
+  },
+
+  {
+    id: 2,
+    nombre: "pizza de Cebolla",
+    precio: 1500,
+    ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+    imagen: "./img/cebolla.png",
+  },
+
+  {
+    id: 3,
+    nombre: "pizza 4 Quesos",
+    precio: 1380,
+    ingredientes: [
+      "Muzzarella",
+      "Tomate",
+      "Queso Azul",
+      "Parmesano",
+      "Roquefort",
+    ],
+    imagen: "./img/4quesos.png",
+  },
+
+  {
+    id: 4,
+    nombre: "pizza Especial",
+    precio: 1000,
+    ingredientes: ["Muzzarella", "Tomate", "Rucula", "Jamón"],
+    imagen: "./img/especial.png",
+  },
+
+  {
+    id: 5,
+    nombre: "pizza con Anana",
+    precio: 600,
+    ingredientes: ["Muzzarella", "Tomate", "Anana"],
+    imagen: "./img/anana.png",
+  },
+];
+
+//CAPTURO LOS ELEMENTOS NECESARIOS
+const form = document.getElementById('form');
+const caja = document.getElementById('container-pizzas');
+const input = document.getElementById('buscador');
+
+
+let items = [];
+
+const renderItems = () => {
+  caja.innerHTML = items.map((item) => {
+    return `
+    <p>${item}</p>
+    `
+  }).join('');
+};
+
+//REALIZAR EL EVENTO PREVENT DEFAULT PARA QUE NOSE PIERDA LA INFORMACION
+//RECORDAR PASAR EL EVENTO (e)
+const submitHandler = (e) => {
+  e.preventDefault();
+  let item = input.value;
+  //Agregar el valor ingresado al array vacio 
+  items = []
+  items = [
+    item
+  ];
+  //Resetear el input para que quede vacio
+  input.value = '';
+
+  renderItems();
+};
+
+//FUNCION INICIADORA
+const init = () => {
+  form.addEventListener('submit', submitHandler);
+
+};
+init();
